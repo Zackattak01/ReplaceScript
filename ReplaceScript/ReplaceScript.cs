1dff2794d89208799b3cc80cd45950fa6195f7d0
Zack Broderson <zbroderson@hotmail.com>
2020-11-12 14:00:06 -0500

    First Commit

---
+++ ReplaceScript/ReplaceScript.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReplaceScript
+{
+    public class ReplaceScript
+    {
+        public ReplaceScript Create()
+        {
+            return new ReplaceScript();
+        }
+    }
+}
